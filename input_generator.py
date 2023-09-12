580dce5926380de05c108976a72c8e969346c5c8
David Gray <dagray@redhat.com>
2023-09-12 16:39:44 -0400

    Rework input generator for multiplexed requests

--- input_generator.py
+++ input_generator.py
@@ -4,6 +4,7 @@
 """
 
 import numpy as np
+import random
 
 from base import Base
 
@@ -22,7 +23,7 @@ class InputGenerator(Base):
         input_json = self._json_load(file)
         self.dataset = input_json["dataset"]
         if max_size is not None:
-            self.dataset = self._get_subset(max_size)
+            self.dataset = self._get_even_subset(max_size)
 
         self.dataset_version = input_json["metadata"]["version"]
         self.next_query = 0
@@ -40,8 +41,16 @@ class InputGenerator(Base):
         Access current dataset
         """
         return self.dataset
+    
+    def get_input_array(self):
+        ret = []
+        for data_object in self.dataset:
+            ret.append(data_object.get("input"))
+        
+        return [data_object.get("input") for data_object in self.dataset]
+            
 
-    def _get_subset(self, max_size):
+    def _get_even_subset(self, max_size):
         """
         Returns a subset of n queries, equally spaced from current self.queries
 
@@ -52,3 +61,11 @@ class InputGenerator(Base):
         indices = np.round(np.linspace(
             0, query_list_length - 1, max_size)).astype(int)
         return ([self.dataset[i] for i in indices])
+
+    def _get_random_subset(self, max_size):
+        """
+        Returns a random subset of n queries of this input generators dataset
+        """
+        ret = self.dataset.copy()
+        random.shuffle(ret)
+        return (ret[0:max_size])

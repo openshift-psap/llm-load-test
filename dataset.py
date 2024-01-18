e73556bdd9de51fbdeb16f0a97276c504adf4dca
David Gray <dagray@redhat.com>
2024-01-18 17:19:32 -0500

    Fix Containerfile

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import json
 import random
+import logging
 
 dataset_seed = 1337
 class Dataset:
@@ -26,19 +27,19 @@ def read_jsonlines(filename, max_queries=1000, max_input_tokens=1024, max_output
             try:
                 json_object = json.loads(line.strip())
             except json.JSONDecodeError as e:
-                print(f"Error decoding JSON in file {filename} {e}")
+                logging.error("Error decoding JSON in file %s %s", filename, e)
                 continue
             try:
-                input_tokens = json_object.get('tok_input_length')
-                input_data = json_object.get('input')
-                output_tokens = input_data.get('min_new_tokens')
+                input_tokens = json_object['tok_input_length']
+                input_data = json_object['input']
+                output_tokens = input_data['min_new_tokens']
             except KeyError as e:
-                print(f"Unexpected format in input dataset file {filename}, KeyError: {e}")
+                logging.error("Unexpected format in dataset file %s, KeyError: %s", filename, e)
+                continue
+                # TODO exit or just skip here?
             if output_tokens < max_output_tokens and input_tokens < max_input_tokens: 
                 total_queries = total_queries + 1
                 input_data["input_tokens"] = input_tokens
                 yield input_data
                 if total_queries >= max_queries:
                     break
-
-

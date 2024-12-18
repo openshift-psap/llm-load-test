1b1c8396ec6669f966a1301387eb76c51885880f
thameem-abbas <thibrahi@redhat.com>
2024-12-18 10:04:57 -0500

    Update : Metadata dict to reflect the synthetic nature of the data and tokenizer source

--- contrib/synthetic_datagen.py
+++ contrib/synthetic_datagen.py
@@ -7,7 +7,7 @@ import json
 SYS_PROMPT = "You are an AI assistant that helps people find information. User will you give you a question. Your task is to answer as faithfully as you can. While answering think step-bystep and justify your answer."
 
 metadata_dict = {
-    "name": "openorca-subset", 
+    "name": "synthetic-data", 
     "version": "0.1.1", 
     "license": "MIT License\n\nCopyright (c) [year] [fullname]\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.\n"
 }
@@ -61,6 +61,7 @@ if __name__ == "__main__":
     dataset = make_dataset(args.model, args.num_samples, args.input_min, args.input_max, args.output_min, args.output_max)
 
     with open(args.dataset_name + "_synthetic.jsonl", "w") as f:
+        metadata_dict["tokenizer"] = args.model
         json.dump(metadata_dict, f)
         f.write("\n")
         for item in dataset:

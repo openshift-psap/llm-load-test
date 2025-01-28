ac44acb750b78f4ce65643b0cb747e05ea813e9d
Samuel Monson <smonson@redhat.com>
2025-01-28 17:16:40 -0500

    Update CLI

--- contrib/synthetic_datagen/synthetic_datagen.py
+++ contrib/synthetic_datagen/synthetic_datagen.py
@@ -1,3 +1,4 @@
+import io
 import os
 from tokenizers import Tokenizer
 import random
@@ -24,7 +25,7 @@ metadata_dict = {
 
 # Generate input and output lengths as 2 independant distributions
 # Future item: Possible dependent distribution
-def gen_io_lengths(num_samples :  int, distribution : str, other_args):
+def gen_io_lengths(num_samples :  int, input_distribution : str, output_distribution : str, other_args):
     Logger.debug(f"Data Random Seed : {DATA_RANDOM_SEED}")
     random_generator = np.random.default_rng(seed=DATA_RANDOM_SEED)
 
@@ -32,24 +33,27 @@ def gen_io_lengths(num_samples :  int, distribution : str, other_args):
     # Equal     - No change in lengths
     # Uniform   - Uniform distribution across the min_max intervals
     # Normal    - Standard normal distribution centered over the mean
-    if distribution == "uniform":
-        return (
-            random_generator.uniform(low=other_args["input_min"], high=other_args["input_max"], size=num_samples),
-            random_generator.uniform(low=other_args["output_min"], high=other_args["output_max"], size=num_samples),
-        )
-    elif distribution == "normal":
-        return (
-            random_generator.normal(loc=other_args["input_mean"], scale=other_args["input_sd"], size=num_samples),
-            random_generator.normal(loc=other_args["output_mean"], scale=other_args["output_sd"], size=num_samples),
-        )
-    elif distribution == "equal":
+    if input_distribution == "uniform":
+        input = random_generator.uniform(low=other_args["input_min"], high=other_args["input_max"], size=num_samples)
+    elif input_distribution == "normal":
+        input = random_generator.normal(loc=other_args["input_mean"], scale=other_args["input_sd"], size=num_samples)
+    elif input_distribution == "equal":
         # Using the library for consistency
-        return (
-            random_generator.normal(loc=other_args["input_len"], scale=0, size=num_samples),
-            random_generator.normal(loc=other_args["output_len"], scale=0, size=num_samples),
-        )
+        input = random_generator.normal(loc=other_args["input_len"], scale=0, size=num_samples)
     else:
-        raise RuntimeError("Unknown distribution requested : " + str(args.distribution))
+        raise RuntimeError("Unknown distribution requested : " + str(input_distribution))
+
+    if output_distribution == "uniform":
+        output = random_generator.uniform(low=other_args["output_min"], high=other_args["output_max"], size=num_samples)
+    elif output_distribution == "normal":
+        output = random_generator.normal(loc=other_args["output_mean"], scale=other_args["output_sd"], size=num_samples)
+    elif output_distribution == "equal":
+        # Using the library for consistency
+        output = random_generator.normal(loc=other_args["output_len"], scale=0, size=num_samples)
+    else:
+        raise RuntimeError("Unknown distribution requested : " + str(output_distribution))
+
+    return input, output
 
 def load_corpus():
     with fileinput.input(files=glob(CORPUS_GLOB), mode='r') as f:
@@ -73,22 +77,26 @@ def make_one_sample(corpus, offsets, req_sample_size : int):
 def make_dataset(args):
 
     model = args['model']
-    num_samples = args['num_samples']
+    num_samples = args['samples']
 
     dataset_info = {
         'model': model,
         'num_samples': num_samples,
-        'distribution': args['distribution']
+        'input_distribution': args['input_distribution'],
+        'output_distribution': args['output_distribution'],
     }
 
     input_lengths, output_lengths = gen_io_lengths(
         num_samples=num_samples, 
-        distribution=dataset_info['distribution'],
+        input_distribution=dataset_info['input_distribution'],
+        output_distribution=dataset_info['output_distribution'],
         other_args=args
         )
 
     # dtype conversion due to output type of random sampling
     input_lengths, output_lengths = input_lengths.astype(dtype=int).tolist(), output_lengths.astype(dtype=int).tolist()
+    assert isinstance(input_lengths, list) # TODO
+    assert isinstance(output_lengths, list) # TODO
     Logger.debug(f"Input and Output lengths : {list(zip(input_lengths, output_lengths))}")
     
     dataset_info["io_lengths"] = list(zip(input_lengths, output_lengths))
@@ -115,56 +123,57 @@ def make_dataset(args):
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser("Create Synthetic Datasets for use with llm-load-test")
-    parser.add_argument("--model", required=True)
-    parser.add_argument("--dataset_name", required=True)
-    parser.add_argument("--num_samples", type = int, default=100)
-    parser.add_argument("--distribution", choices=["normal", "uniform", "equal"], required=True)
+    parser = argparse.ArgumentParser(
+        description="Create Synthetic Datasets for use with llm-load-test",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-m", "--model",
+        help="Huggingface model name or path to model",
+        required=True,
+    )
+    parser.add_argument("-o", "--dataset", metavar="FILE", required=True, type=argparse.FileType('w', encoding='UTF-8'))
+    parser.add_argument("-c", "--samples", metavar="COUNT", type=int, default=100)
 
-    # Equal length Params
-    parser.add_argument("--input_len", type=int)
-    parser.add_argument("--output_len", type=int)
+    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group.add_argument("--input-equal", metavar="LEN", type=int, nargs=1)
+    input_group.add_argument("--input-normal", metavar=("MEAN", "SD"), type=int, nargs=2)
+    input_group.add_argument("--input-uniform", metavar=("MIN", "MAX"), type=int, nargs=2)
 
-    # Normal Distribution Params
-    parser.add_argument("--input_mean", type=int)
-    parser.add_argument("--input_sd", type=int)
-    parser.add_argument("--output_mean", type=int)
-    parser.add_argument("--output_sd", type=int)
-
-    # Uniform Distribution Params
-    parser.add_argument("--input_min", type = int)
-    parser.add_argument("--input_max", type = int, default=None)
-    parser.add_argument("--output_min", type = int)
-    parser.add_argument("--output_max", type = int, default=None)
+    output_group = parser.add_mutually_exclusive_group(required=True)
+    output_group.add_argument("--output-equal", metavar="LEN", type=int, nargs=1)
+    output_group.add_argument("--output-normal", metavar=("MEAN", "SD"), type=int, nargs=2)
+    output_group.add_argument("--output-uniform", metavar=("MIN", "MAX"), type=int, nargs=2)
 
     args = parser.parse_args()
 
     arg_vars = vars(args)
 
-    if args.distribution == "uniform":
-        uniform_params = ("input_min", "input_max", "output_min", "output_max")
-        if not all(arg_vars[k] != None for k in uniform_params):
-            parser.error("Missing fields : " + str([k for k in uniform_params if arg_vars[k] is None]))
-    elif args.distribution == "normal":
-        normal_params = ("input_mean", "input_sd", "output_mean", "output_sd")
-        if not all(arg_vars[k] != None for k in normal_params):
-            parser.error("Missing fields : " + str([k for k in normal_params if arg_vars[k] is None]))
-    elif args.distribution == "equal":
-        equal_params = ("input_len", "output_len")
-        if not all(arg_vars[k] != None for k in equal_params):
-            parser.error("Missing fields : " + str([k for k in equal_params if arg_vars[k] is None]))
-    else:
-        raise RuntimeError("Unknown distribution requested : " + str(args.distribution))
+    for i in ("input", "output"):
+        if arg_vars.get(f"{i}_uniform") != None:
+            params = ("min", "max")
+            argv: list = arg_vars[f"{i}_uniform"]
+            arg_vars[f"{i}_distribution"] = "uniform"
+        elif arg_vars.get(f"{i}_normal") != None:
+            params = ("mean", "sd")
+            argv: list = arg_vars[f"{i}_normal"]
+            arg_vars[f"{i}_distribution"] = "normal"
+        elif arg_vars.get(f"{i}_equal") != None:
+            params = ("len",)
+            argv: list = arg_vars[f"{i}_equal"]
+            arg_vars[f"{i}_distribution"] = "equal"
+        else:
+            raise RuntimeError(f"Unknown distribution requested for: {i}")
+        arg_vars.update({f"{i}_{k}": v for k,v in zip(params, argv)})
 
     dataset, dataset_info = make_dataset(arg_vars)
     metadata_dict['dataset_info'] = dataset_info
 
-    dataset_file_name = args.dataset_name + "_synthetic.jsonl"
-    with open(dataset_file_name, "w") as f:
-        json.dump(metadata_dict, f)
+    f: io.TextIOWrapper = args.dataset
+    json.dump(metadata_dict, f)
+    f.write("\n")
+    for item in dataset:
+        json.dump(item, f)
         f.write("\n")
-        for item in dataset:
-            json.dump(item, f)
-            f.write("\n")
 
-    Logger.info(f"Dataset saved to : {dataset_file_name}")
+    Logger.info(f"Dataset saved to : {f.name}")
